df31713cf9464abdfdb75e964f549ceeab429427
minsikmoon <msmoon.business@gmail.com>
2019-04-06 23:27:27 +0900

    assertEquals

--- test/com/in28minute/junit5/StringTest.java
+++ test/com/in28minute/junit5/StringTest.java
@@ -8,7 +8,23 @@ class StringTest {
 
 	@Test
 	void tc01_firstRedBar() { //메소드 이름은 짓고 싶은거 아무거나
-		fail("Not yet implemented");
+		//fail("Not yet implemented"); //fail이 없다면 success와 같다.
+		//Write test code
+		//invoke method squar(4)
+		//check in place = 16
+		//결국 체크를 해야한다.
+		//체크를 junit에서 assertions라고 부른다.
 	}
+	
+	@Test
+	void tc02_lengthTest() { 
+		int actualLength = "abcd".length();
+		int expectedLength = 4;
+		assertEquals(expectedLength, actualLength);
+	}
+	
+	
+	
+	
 
 }
