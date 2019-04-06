8ecb10479dd091f9a3a077e025bd7642631afe7e
minsikmoon <msmoon.business@gmail.com>
2019-04-06 23:10:24 +0900

    first red bar

---
+++ src/com/in28minutes/junit5/StringTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes.junit5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
