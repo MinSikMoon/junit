6187866857138b717b95897f929920e83f208fbd
minsikmoon <msmoon.business@gmail.com>
2019-04-07 12:06:00 +0900

    assert throws

--- test/com/in28minute/junit5/StringTest.java
+++ test/com/in28minute/junit5/StringTest.java
@@ -14,59 +14,63 @@ class StringTest {
 	static void beforeAll() {
 		System.out.println("initialize connection to database");
 	}
-	
+
 	@AfterAll
 	static void afterAll() {
 		System.out.println("close connection to database");
 	}
-	
-	@BeforeEach //@Before in junit4
-	void beforeEach(TestInfo info) { //junit4에서는 없는 객체
+
+	@BeforeEach // @Before in junit4
+	void beforeEach(TestInfo info) { // junit4에서는 없는 객체
 		System.out.println("initialize test data for each test" + info.getDisplayName());
 	}
-	
-	@AfterEach //@After in junit4
+
+	@AfterEach // @After in junit4
 	void afterEach() {
 		System.out.println("clean up test data");
 	}
 
 	@Test
-	void tc01_firstRedBar() { //메소드 이름은 짓고 싶은거 아무거나
-		//fail("Not yet implemented"); //fail이 없다면 success와 같다.
-		//Write test code
-		//invoke method squar(4)
-		//check in place = 16
-		//결국 체크를 해야한다.
-		//체크를 junit에서 assertions라고 부른다.
+	void tc01_firstRedBar() { // 메소드 이름은 짓고 싶은거 아무거나
+		// fail("Not yet implemented"); //fail이 없다면 success와 같다.
+		// Write test code
+		// invoke method squar(4)
+		// check in place = 16
+		// 결국 체크를 해야한다.
+		// 체크를 junit에서 assertions라고 부른다.
 	}
-	
+
 	@Test
-	void tc02_lengthTest() { 
+	void tc02_lengthTest() {
 		assertEquals(4, "abcd".length());
 	}
-	
+
 	@Test
-	void tc03_toUpperCase() { 
+	void tc03_toUpperCase() {
 		assertEquals("ABCD", "abcd".toUpperCase());
 		assertNotNull("abcd".toUpperCase());
 	}
-	
+
 	@Test
 	void tc04_contains() {
 		assertEquals(false, "abcdefg".contains("i jk"));
 		assertFalse("abcdefg".contains("i jk"));
 	}
-	
+
 	@Test
 	void tc05_split() {
 		String str = "abc def ghi";
 		String actual[] = str.split(" ");
-		String[] expected = new String[] {"abc", "def", "ghi"};
+		String[] expected = new String[] { "abc", "def", "ghi" };
 		assertArrayEquals(expected, actual);
 	}
-	
-	
-	
-	
+
+	@Test
+	void tc06_exception() {
+		String str = null;
+		assertThrows(NullPointerException.class, () -> {
+			str.toUpperCase(); //exception을 던지는 메소드를 작성한다.
+		});
+	}
 
 }
